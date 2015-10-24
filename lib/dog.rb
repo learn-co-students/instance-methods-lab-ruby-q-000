459ebef5bb21466390ee6c4f0bef652ce09c5665
ElnurIbrahimzade <elnur.ibrahim-zade@hotmail.com>
2015-10-24 21:16:48 +0000

    nitrous-backup at Sat Oct 24 21:16:48 UTC 2015

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,21 @@ class Dog
     puts "The Dog is sitting"
   end
 
+  def name=(dog_name)
+    @this_dog_name=dog_name
+  end
+
+  def name
+    @this_dog_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+
+
+
+
+
