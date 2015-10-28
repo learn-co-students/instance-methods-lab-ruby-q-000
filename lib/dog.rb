acd78be50479d4fd217229a0b4b324b33659ecf8
JVThomas <jthomas0209@gmail.com>
2015-10-28 15:04:07 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+  
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The person is walking"
+  end
+end
