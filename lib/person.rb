2ee8af245eed7ab29249f4cfa6012e960b3d93a2
mphinn <mariaphinney@gmail.com>
2016-01-25 07:35:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+    end
+end
+
+adele_goldberg = Person.new
+adele_goldberg.talk
+adele_goldberg.walk
