56d045d8a55df291fc65ec568f46f561ea228c3a
hangloos <ericloos00@gmail.com>
2016-02-01 19:09:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,6 @@ class Dog
     puts "Woof!"
     end
   def sit
-    puts "The dog is sitting"
-  end
+    puts "The Dog is sitting"
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,6 @@ class Person
     puts "Hello World!"
   end
   def walk
-    puts "The person is walking"
+    puts "The Person is walking"
+  end
 end
