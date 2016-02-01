ffcda9c28813c7c0315d60e87834081787de4fdc
hangloos <ericloos00@gmail.com>
2016-02-01 19:02:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  def bark
+    puts "Woof!"
+    end
+  def sit
+    puts "The dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The person is walking"
+end
