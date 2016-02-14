15e18ac7499dfbd80a024e9b03127d1cd07830e1
aback1227 <aback1227@gmail.com>
2016-02-13 22:50:18 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,13 @@
 class Dog
 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+
   def bark
     puts "Woof!"
   end
@@ -10,3 +18,7 @@ class Dog
 
 end
 
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
