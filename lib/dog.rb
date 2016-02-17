db8ca8d93ccf52f022785a0297a097156fdf80f6
layups159 <amauris.quezada@gmail.com>
2016-02-17 16:27:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+end
+
+def bark
+  puts "Woof!"
+end
+
+def sit
+  "The Dog is sitting"
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+end
+def talk
+  puts "Hello World!"
+end
+def walk
+  puts "The Person is walking"
+end
