b07d65fc384d398f0a3ebeff48b1012adcc73695
layups159 <amauris.quezada@gmail.com>
2016-02-17 16:30:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-end
 
 def bark
   puts "Woof!"
 end
 
 def sit
-  "The Dog is sitting"
+  puts "The Dog is sitting"
+end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
-end
+
 def talk
   puts "Hello World!"
 end
 def walk
   puts "The Person is walking"
+end
 end
