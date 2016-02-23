e74047c97904048cba362403fb935919a8e23787
kade042 <dereje88@gmail.com>
2016-02-23 21:37:17 +0000

    nitrous-backup at Tue Feb 23 21:37:16 UTC 2016

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,20 @@
 class Dog
+  def name=(dog_name)
+    
+    @this_dog_name = dog_name
+  end
+  def name
+    @this_dog_name
+  end
   def bark
     puts "Woof!"
   end
-  
+
   def sit
     puts "The Dog is sitting"
   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
