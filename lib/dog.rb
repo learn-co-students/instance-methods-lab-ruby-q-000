bce2965bb571d24923cb74bb153e0db8ac0a0746
rublen <rublen77@gmail.com>
2016-03-03 20:46:36 +0000

    nitrous-backup at Thu Mar  3 20:46:36 UTC 2016

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,11 @@
 class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
+  def name
+    @this_dogs_name
+  end
   def bark
     puts "Woof!"
   end
@@ -13,3 +19,5 @@ end
 fido = Dog.new
 snoopy = Dog.new
 lassie = Dog.new
+lassie.name= "Lassie"
+  puts lassie.name
