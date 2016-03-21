cd91c4612891f14f338b52ede3e5126ba6b92756
anorico <allan.norico@gmail.com>
2016-03-21 13:23:55 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+  #talk
+  def talk
+    puts "Hello World!"
+  end
+
+  #walk
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
