b5c5820ac723ef18667d2e8af198b16f13676814
avanover0311 <avanover0311@gmail.com>
2016-04-20 19:22:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    def bark
+    puts "Woof!"
+  def sit
+    puts "The Dog is sitting"
+  end
+    end
+
+end
+
+fido = Dog.new
+  fido.bark
+  fido.sit
+  snoopy = Dog.new
+  snoopy.bark
+  snoopy.sit
+  lassie = Dog.new
+  lassie.bark
+  lassie.sit
