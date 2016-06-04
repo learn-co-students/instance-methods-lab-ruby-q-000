01b90fa80e388f17537e8cbf18c1eeeb5633c9f7
vanita eckert <vanita112003@gmail.com>
2016-06-04 19:28:35 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  lassie = Dog.new
+  lassie.bark
+  lassie.sit
+
+  snoopy = Dog.new
+  snoopy.bark
+  snoopy.sit
+
+  fido = Dog.new
+  fido.bark
+    fido.sit
+
+end
