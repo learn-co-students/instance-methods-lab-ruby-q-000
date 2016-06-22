842b25c8d0931077f94da9f44c71df73cf882be1
Ellen Ades <ellenades@gmail.com>
2016-06-22 21:26:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+end
+Fido.new
+Snoopy.new
+Lassie.new
+end
+class Dog
+end
+class Dog
+  def bark
+  puts "woof"
+end
+end
+class Dog
+  def sit
+    puts "The dog is sitting."
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+end
+def talk
+end
+class Person
+  def talk
+    puts "Hello World"
+  end
+end
