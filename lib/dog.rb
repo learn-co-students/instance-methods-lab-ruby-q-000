8f1d4f3bd8d173c92a9c5abd59fe76ccec4ff67b
Danyale Walker <danyalecw@hotmail.com>
2016-06-28 23:02:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def bark
+    puts. "Woof!"
+  end
+end
+class Dog
+  def sit
+    puts. "Sitting!"
+  end
+end
+
+fido = dog.new
+fido.bark
+fido.sit
+
+snoopy = dog.new
+snoopy.bark
+snoopy.sit
+
+lassie = dog.new
+lassie.bark
+lassie.sit
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def talk
+    puts. "Hello World!"
+  end
+end
+class Person
+  def walk
+    puts. "The Person Is Walking!"
+  end
+end
+  adele_goldberg = person.new
+adele_goldberg.talk
+adele_goldberg.walk
+
+  alan_kay = person.new
+  alan_kay.talk
+  alan_kay.walk
